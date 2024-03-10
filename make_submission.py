c65c61f81e937fb8b449ec4d5940e7c8f5c0de29
Administrator <admin@example.com>
2024-03-10 10:13:21 +0000

    Update make_submission.py

--- make_submission.py
+++ make_submission.py
@@ -1,10 +1,9 @@
+import pandas as pd
 from baseline import make_pedictions
 
 TEST_DATASET = "./data/private_info/test_df.parquet"
 SUBMISSION_PATH = "./data/submission.csv"
 
 if __name__ == "__main__":
-    predictions = make_pedictions(TEST_DATASET)
-    with open(SUBMISSION_PATH, "w") as f:
-        for line in predictions:
-            f.write(f"{line}\n")
+    predictions = pd.Series(make_pedictions(TEST_DATASET))
+    predictions.to_csv(SUBMISSION_PATH, index=False, header=True)
